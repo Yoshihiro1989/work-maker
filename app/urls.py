731278973e7ccc4abda3ed0a117da13908a96d9c
Yhoshihiro <ykajimoto0@gmail.com>
2020-02-01 10:51:41 +0900

    Work-maker

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = "app"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("home/", views.home, name="home"),
+    path('signup/', views.signup, name='signup'),
+    path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
+    path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
+    path('users/<int:pk>/delete/', views.UserDeleteView.as_view(), name='users_delete'),
+    path("lists/create/", views.ListCreateView.as_view(), name="lists_create"),
+    ]
---
+++ app/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render,redirect,resolve_url
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.generic import DetailView,UpdateView,DeleteView,CreateView
+from .form import UserForm,ListForm
+from .mixins import OnlyYouMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from . models import List
+# Create your views here.
+
+
+def index(request):
+    return render(request, "app/index.html")
+
+def home(request):
+    return render(request, "app/home.html")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("app:home")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'app/signup.html', context)
+@login_required
+def home(request):
+    return render(request, "app/home.html")
+
+class UserDetailView(LoginRequiredMixin,DetailView):
+    model = User
+    template_name = "app/users/detail.html"
+
+
+class UserUpdateView(OnlyYouMixin,UpdateView):
+    model = User
+    template_name = "app/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('app:users_detail', pk=self.kwargs['pk'])
+
+class UserDeleteView(OnlyYouMixin,DeleteView):
+    model = User
+    template_name = "app/users/delete.html"
+
+    def get_success_url(self):
+        return resolve_url('app:users_detail', pk=self.kwargs['pk'])
+
+class ListCreateView(LoginRequiredMixin, CreateView):
+    model = List
+    template_name = "app/lists/create.html"
+    form_class = ListForm
+    success_url = reverse_lazy("app:home")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
